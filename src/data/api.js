eea5a8eda97d6cb39ff6b176dbe92ae2cbabbc15
Gustavo <guscooke14@gmail.com>
2021-10-07 23:05:08 -0300

    upload

--- src/data/api.js
+++ src/data/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const key = 'AIzaSyCDWfrscG4in4lu9oKgxTJWXYKR2bkxgmw'
+// export const key = 'AIzaSyCDWfrscG4in4lu9oKgxTJWXYKR2bkxgmw'
 
 const api = axios.create({
     baseURL: 'https://www.googleapis.com/books/v1/volumes?q='
